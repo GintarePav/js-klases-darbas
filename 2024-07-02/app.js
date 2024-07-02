c35badec857c38fdfb016e403aeef5e4e654797c
GintarePav <gintare.pavilaviciute@mail.com>
2024-07-02 12:39:56 +0300

    node.js

---
+++ 2024-07-02/app.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const url = require("url");
+// const sayHello = require("./modules/sayHello");
+
+let product = fs.readFileSync("./data/products.json", "utf-8"); // skaito
+product = JSON.parse(product);
+
+// sayHello();
+
+// Create server:
+
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+  console.log(pathname);
+  switch (pathname) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>This is home.</h1>");
+      break;
+    case "/api/products":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+      break;
+    case "/api/product":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product[query.id]));
+      break;
+    case "/api/search/name":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product[query.productName]));
+    default:
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Page not found</h1>");
+      break;
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server listening on port 8000");
+});
---
+++ 2024-07-02/modules/sayHello.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+  console.log("Hello");
+};
+
+/*
+API sukurti nauja endpoint:
+/api/search/name
+
+Jo pagalba galima surasti produkta pagal pavadinima.
+
+url: http://127.0.0.1/8000/api/search/?name=Stacionarus
+*/
+
+
